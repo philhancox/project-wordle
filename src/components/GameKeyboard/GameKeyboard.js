2888ca4c988b246824ad928ecfcd5ab281c68ee3
Phil Hancox <phil.hancox@revolutionbarsgroup.com>
2024-12-09 12:59:48 +0000

    Better keyboard implementation

--- src/components/GameKeyboard/GameKeyboard.js
+++ src/components/GameKeyboard/GameKeyboard.js
@@ -4,22 +4,14 @@ import GameKeyboardKey from "../GameKeyboardKey";
 function GameKeyboard({ letters }) {
 
   return <div className="keyboard">
-    <div className="keyboard-row">
-      {'QWERTYUIOP'.split('').map((letter, index) => {
-        return <GameKeyboardKey key={index} status={letters[letter]}>{letter}</GameKeyboardKey>
-      })}
-    </div>
-    <div className="keyboard-row">
-      {'ASDFGHJKL'.split('').map((letter, index) => {
-        return <GameKeyboardKey key={index} status={letters[letter]}>{letter}</GameKeyboardKey>
-      })}
-    </div>
-    <div className="keyboard-row">
-      {'ZXCVBNM'.split('').map((letter, index) => {
-        return <GameKeyboardKey key={index} status={letters[letter]}>{letter}</GameKeyboardKey>
-      })}
-    </div>
-  </div>;
+    {['QWERTYUIOP', 'ASDFGHJKL', 'ZXCVBNM'].map((keys) => {
+      return <div className="keyboard-row">
+        {keys.split('').map((letter, index) => {
+          return <GameKeyboardKey key={index} status={letters[letter]}>{letter}</GameKeyboardKey>
+        })}
+      </div>
+    })}
+  </div>
 }
 
 export default GameKeyboard;
